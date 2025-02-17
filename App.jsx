a3faba25bca04b3914b03ed1f9f3e581158e2b20
ThigasMusk <166817648+ThigasMusk@users.noreply.github.com>
2025-02-17 09:35:00 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import HelloWorld from "./HelloWorld";
+import Contador from "./contador";
+import Mensagem from "./Mensagem";
+function App() {
+  return (
+    <div>
+        <HelloWorld/>
+        <Contador/>
+        <Mensagem/>
+    </div>
+  );
+}
+
+export default App;
